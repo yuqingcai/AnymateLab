d9ceb9b9019c600df39113f38c5efdcbfec2a934
yuqingcai <yuqingcai@gmail.com>
2024-09-12 15:46:27 +0800

    render to texture

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QDir::setCurrent("/Users/yuqingcai/");
+    QString appDir = QCoreApplication::applicationDirPath();
+    QDir::setCurrent(QDir(appDir).filePath(".."));
+    qDebug() << "Current working directory (Project Directory):" <<
+        QDir::currentPath();
 
 #if defined(Q_OS_WIN) || defined(Q_OS_LINUX)
     qputenv("QSG_RHI_BACKEND", "opengl");
